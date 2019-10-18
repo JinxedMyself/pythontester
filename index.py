05c048f88221f9e817f8249a7f66b5c9507423b0
JinxedMyself <JinxedMyself@users.noreply.github.com>
2019-10-18 13:17:24 +0200

    Update index.py

--- index.py
+++ index.py
@@ -3,7 +3,23 @@ def application(environ, start_response):
     response_header = [('Content-type', 'text/html')]
     start_response(status, response_header)
 
-    html = 'Hello World'
+    html = ''
+    html += '<html>\n'
+    html += ' <body>\n'
+    html += ' <title>Test WSGI page for fys</title>\n'
+    html += ' <div style="width: 100%; font-size: 20px; font-weight: bold; text-align: center;">\n'
+    html += ' Welcome to the mod_wsgi test page\n'
+    html += ' </div>\n'
+    html += ' <h2>Some interesting environment variables:</h2>\n'
+    html += ' <div style="width: 100%; font-family: Courier; font-size: 14px; font-weight: bold; text-align: left;">\n'
+    environmentVars = ['REQUEST_METHOD', 'REQUEST_URI', 'QUERY_STRING','SCRIPT_NAME', 'HTTP_REFERER']
+    for envVar in environmentVars:
+        envVarValue = environ.get(envVar, '')
+        html += envVar + ' = ' + envVarValue + '<br>\n'
+    html += ' </div>\n'
+    html += ' </body>\n'
+    html += '</html>\n'
+    
     return [bytes(html, 'utf-8')]
 
 if __name__ == '__main__':
